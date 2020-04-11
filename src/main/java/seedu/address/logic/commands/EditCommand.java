eb40e728a728985b4375c32f111d85ba1523c6e6
Branson <branson.ng@u.nus.edu>
2020-04-12 00:13:07 +0800

    update edit command

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -100,7 +100,7 @@ public class EditCommand extends Command {
                     .setTaskInProgressText(editedTask.getName().toString());
         }
 
-        model.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
+        model.showAllTasks();
         // editedTask.triggerRecurringIfPresent(model, index);
         return new CommandResult(String.format(MESSAGE_EDIT_TASK_SUCCESS, editedTask));
     }
